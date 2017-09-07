014c194a04966e2b1a39e9d3dd75361eb76a305f
Josh D. Carvin <j.d.carvin@gmail.com>
2017-09-07 16:14:15 -0400

    added :project_id to story_params in StoriesController

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -69,6 +69,6 @@ class StoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_params
-      params.require(:story).permit(:in_order_to, :as, :i_want_to, :i_do, :this_happens, :importance, :comments, :dev_comments, :difficulty, :story_kind, :author)
+      params.require(:story).permit(:in_order_to, :as, :i_want_to, :i_do, :this_happens, :importance, :comments, :dev_comments, :difficulty, :story_kind, :author, :project_id)
     end
 end
