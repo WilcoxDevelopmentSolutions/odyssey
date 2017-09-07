1825a439f530fb9581b6a52cc78175ea189820d3
Josh D. Carvin <j.d.carvin@gmail.com>
2017-09-07 16:08:08 -0400

    added migration for reference between story and project. Added handler to story form for selecting a project to associate with. Added has_many :stories to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   has_many :stories
   has_many :team_members
   has_many :users, through: :team_members
+  belongs_to :author, class_name: User
 
   def current_team_members
     self.team_members.current
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901191435) do
+ActiveRecord::Schema.define(version: 20170907193852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170901191435) do
     t.integer "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_stories_on_project_id"
   end
 
   create_table "team_members", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 20170901191435) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "stories", "projects"
 end
