98b2a08e6c2710d4018d9c1c6179de69f0b19445
Ryan Wilcox <rwilcox@wilcoxd.com>
2017-09-09 20:34:58 -0400

    add migration adding missed db fields to team members

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907193852) do
+ActiveRecord::Schema.define(version: 20170909235253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 20170907193852) do
     t.integer "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_team_members_on_project_id"
+    t.index ["user_id"], name: "index_team_members_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +61,6 @@ ActiveRecord::Schema.define(version: 20170907193852) do
   end
 
   add_foreign_key "stories", "projects"
+  add_foreign_key "team_members", "projects"
+  add_foreign_key "team_members", "users"
 end
