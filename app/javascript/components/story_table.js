5c353914e0c416fbcabdf18b81da799e68b1a4bd
Josh D. Carvin <j.d.carvin@gmail.com>
2017-10-23 08:57:27 -0400

    Boilerplate React component added to app/javascript/components/story_table.js

--- app/javascript/components/story_table.js
+++ app/javascript/components/story_table.js
@@ -1,14 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import RaisedButton from 'material-ui/RaisedButton';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class StoryTable extends React.Component {
   render() {
       return <MuiThemeProvider>
 		<div>
-			<RaisedButton label={this.props.displayName} primary={true} style={{margin: 12}} />
+			  <Table>
+			    <TableHeader>
+			      <TableRow>
+			        <TableHeaderColumn>ID</TableHeaderColumn>
+			        <TableHeaderColumn>Name</TableHeaderColumn>
+			        <TableHeaderColumn>Status</TableHeaderColumn>
+			      </TableRow>
+			    </TableHeader>
+			    <TableBody>
+			      <TableRow>
+			        <TableRowColumn>1</TableRowColumn>
+			        <TableRowColumn>John Smith</TableRowColumn>
+			        <TableRowColumn>Employed</TableRowColumn>
+			      </TableRow>
+			      <TableRow>
+			        <TableRowColumn>2</TableRowColumn>
+			        <TableRowColumn>Randal White</TableRowColumn>
+			        <TableRowColumn>Unemployed</TableRowColumn>
+			      </TableRow>
+			      <TableRow>
+			        <TableRowColumn>3</TableRowColumn>
+			        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+			        <TableRowColumn>Employed</TableRowColumn>
+			      </TableRow>
+			      <TableRow>
+			        <TableRowColumn>4</TableRowColumn>
+			        <TableRowColumn>Steve Brown</TableRowColumn>
+			        <TableRowColumn>Employed</TableRowColumn>
+			      </TableRow>
+			      <TableRow>
+			        <TableRowColumn>5</TableRowColumn>
+			        <TableRowColumn>Christopher Nolan</TableRowColumn>
+			        <TableRowColumn>Unemployed</TableRowColumn>
+			      </TableRow>
+			    </TableBody>
+			  </Table>
 		</div>
       </MuiThemeProvider>
   }
