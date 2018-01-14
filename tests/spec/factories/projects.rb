bd7126a8140b51bb969a0521a44435cd8d919ddf
Ryan Wilcox <rwilcox@wilcoxd.com>
2018-01-13 22:00:49 -0500

    fix tests wrt new validations devise added

--- tests/spec/factories/projects.rb
+++ tests/spec/factories/projects.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :project_with_author, class: Project do
     name "Odyssey"
     
-    association :author, factory: :user
+    association :author, factory: :user, email: 'founder@example.com'
   end
 end
--- tests/spec/factories/users.rb
+++ tests/spec/factories/users.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     last_name "Astaire"
     email "fred.astaire@example.com"
     role 1
+    password "passw0rd!"
+  end
+
+  factory :user_founder do
+    username "fred"
+    first_name "Fred"
+    last_name "Founder"
+    email "fred.founder@example.com"
+    role 1
+    password "passw0rd!"
   end
 end
